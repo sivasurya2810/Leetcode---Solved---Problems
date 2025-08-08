b51559c78c90e56d21cde72cfe7c1c863d0a6284
sivasurya2810 <c.s.sivasurya2004@gmail.com>
2025-08-08 15:54:23 +0530

    Time: 2 ms (91.56%), Space: 44.7 MB (22.13%) - LeetHub

---
+++ 3533-snake-in-matrix/3533-snake-in-matrix.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int finalPositionOfSnake(int n, List<String> commands) {
+        int i=0,j=0;
+        for(String s:commands){
+            char ch=s.charAt(0);
+            if(ch=='R')j++;
+            else if(ch=='L')j--;
+            else if(ch=='U')i--;
+            else i++;
+            }
+            return (i*n)+j;
+
+    }
+}
